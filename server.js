7c5aadb4d5a663d63233d1252297e3ad5d4ef61b
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-07 16:05:25 -0800

    weeww

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const port = process.env.PORT;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use('/angular-files');
+
+app.get('/', function(req, res) {
+  res.render('index.html')
+});
+
+app.use(express.static(__dirname + '/angular-files'));
 
 app.get('/angular-files', function (req, res) {
   res.render('index.html')

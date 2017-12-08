6291f24e7d08770e8751ed53fc373634ef3b42e0
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-07 21:59:57 -0800

    final commit

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/home.html'));
 });
 
+app.get('/angular', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/angular.html'));
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
